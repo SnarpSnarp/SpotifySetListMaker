8c670947bc71148e8d189f5db5ac12925da391ec
SnarpSnarp <57366569+SnarpSnarp@users.noreply.github.com>
2023-08-01 18:41:56 -0400

    Update Key.py
    Signed-off-by: SnarpSnarp <57366569+SnarpSnarp@users.noreply.github.com>

--- Key.py
+++ Key.py
@@ -1,5 +1,5 @@
 SETLIST_API_KEY = '###'  # Replace this with your actual API key
 SPOTIPY_CLIENT_ID = '###'
 SPOTIPY_CLIENT_SECRET = '###'
-SPOTIPY_REDIRECT_URI = '###'  # Set this to a valid redirect URI for your app
+SPOTIPY_REDIRECT_URI = 'http://localhost:8888/callback/' #Must match redirect url specified in spotify app
 SPOTIPY_USERNAME = '###'   # Replace with your Spotify username
